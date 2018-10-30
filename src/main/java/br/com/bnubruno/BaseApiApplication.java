e1ff2a59659b5c5dbcc9fc8f0b05f3e9848a6f0e
Bruno Muehlbauer de Souza <bruno.muehlbauer@contaazul.com>
2018-10-30 13:26:07 -0300

    first commit

---
+++ src/main/java/br/com/bnubruno/BaseApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.bnubruno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run( BaseApiApplication.class, args );
+	}
+}
